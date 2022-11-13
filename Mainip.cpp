1606eb095481939323b2d1901df541051d60fd74
Cuchoox <118080946+Cuchoox@users.noreply.github.com>
2022-11-13 19:42:54 -0300

    Update Mainip.cpp

--- Mainip.cpp
+++ Mainip.cpp
@@ -30,13 +30,38 @@ int main(int argc, char *argvc)
        cont++;
     }
  
+void Check (){
+    string ips 
+    string command = "ping " + ips + pack
+    FILE *pipe = popen(command.c_str(), "r");
+	if(!pipe) cout << "error" << endl;
+}
+
+
+
+
+
+
+
+
+
+
+pclose(pipe);
+
 thread threads <cont>;
+int i = 0;
+for (i: i<cont; i++){
+    threads[i] = thread(ipCheck)
 
+}
+for (i = 0: i<cont; i++){
+    threads[i].join();
 
+}
 
 
  
 
- 
+ return 0;
     system ("pause");
 }
